795f304cac3270b7c5e6e9253c294633a3c52ec4
Bo Peng <bpeng4@login1.swan.hcc.unl.edu>
2025-03-13 11:10:02 -0500

    Fst_Calculate.R

--- slurm-script/Fst_Calculate.R
+++ slurm-script/Fst_Calculate.R
@@ -1,42 +1,22 @@
----
-title: "HW2"
-output: html_document
-date: "2025-03-10"
-name: Bo Peng
-
-output:
-  html_document:
-    df_print: paged
-  word_document: default
----
-
-
 # A procedure to calculate $\theta$ values
-
 ### 3. Calculate the Fst value for each site and visualize the results
 
-
-```{r, eval=FALSE}
 library("data.table")
-geno <- fread("largedata/snp_calls.txt", header=FALSE)
-geno <- geno[, -27]
-names(geno) <- c("chr", "pos", "ref", "alt", "quality", "depth", paste0("l",1:20))
+geno <- fread("/work/agro932/bpeng4/PopGen_HW/largedata/snp_calls.txt", header=FALSE)
+names(geno) <- c("chr", "pos", "ref", "alt", "quality", "depth", 
+                 "SRR11119156", "SRR11119158", "SRR11119163", "SRR11119168", 
+                        "SRR11119169", "SRR11119186", "SRR11119187", "SRR11119188", 
+                        "SRR11119189", "SRR11119190")
+setDT(geno)
 
-
-for(i in 5:24){
-  # replace slash and everything after it as nothing
-  geno$newcol <- gsub("/.*", "", geno[,i] )
-  # extract the line name
+for(i in 7:16){
+  # Extract the column name
   nm <- names(geno)[i]
-  # assign name for this allele
-  names(geno)[ncol(geno)] <- paste0(nm, sep="_a1")
   
-  geno$newcol <- gsub(".*/", "", geno[,i] )
-  names(geno)[ncol(geno)] <- paste0(nm, sep="_a2")
+  # Create new columns for each allele
+  geno[[paste0(nm, "_a1")]] <- gsub("/.*", "", geno[[i]])  # First allele
+  geno[[paste0(nm, "_a2")]] <- gsub(".*/", "", geno[[i]])  # Second allele
 }
-```
-
----
 
 # A procedure to calculate $\theta$ values
 
@@ -55,7 +35,6 @@ geno$p1 <- geno$p1/6
 geno$p2 <- apply(geno[, 47:64], 1, function(x) {sum(as.numeric(as.character(x)))})
 geno$p2 <- geno$p2/4
 ```
-
 Then finally,
 
 ```{r, eval=FALSE}
@@ -80,11 +59,3 @@ write.table(geno, "cache/fst.csv", sep=",", row.names = FALSE, quote=FALSE)
 fst <- read.csv("cache/fst.csv")
 
 plot(fst$pos, fst$fst, xlab="Physical position", ylab="Fst value", main="")
-```
-
-
-
-
-
-
-
