413b0433b615d7b734d56d74b9f9dabc245d52bd
Bo Peng <bpeng4@login1.swan.hcc.unl.edu>
2025-03-17 11:25:57 -0500

    save

--- slurm-script/Fst_Calculate.R
+++ slurm-script/Fst_Calculate.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # A procedure to calculate $\theta$ values
 ### 3. Calculate the Fst value for each site and visualize the results
 
@@ -27,24 +29,16 @@ for(i in 7:16){
 geno[geno == "."] <- NA
 
 # Calculate overall allele frequency (p)
-geno$p <- apply(geno[, 17:36], 1, function(x) {
-  x <- as.numeric(as.character(x))  # Ensure numeric conversion
-  sum(x == 0) / length(x)           # Frequency of allele 0
-})
+geno$p <- apply(geno[, 17:36], 1, function(x) { sum(x==0)/(sum(x==1) + sum(x=0))})
+geno$p <- geno$p/20
 
-# Calculate allele frequencies for two subpopulations
-geno$p1 <- apply(geno[, 17:26], 1, function(x) {
-  x <- as.numeric(as.character(x))
-  sum(x) / length(x)  # Frequency of allele 1
-})
+geno$p1 <- apply(geno[, 17:26], 1, function(x) {sum(as.numeric(as.character(x)))})
+geno$p1 <- geno$p1/10
 
-geno$p2 <- apply(geno[, 27:36], 1, function(x) {
-  x <- as.numeric(as.character(x))
-  sum(x) / length(x)  # Frequency of allele 1
-})
+geno$p2 <- apply(geno[, 27:36], 1, function(x) {sum(as.numeric(as.character(x)))})
+geno$p2 <- geno$p2/10
 
-# Compute Fst using a standard formula
-geno$fst <- with(geno, ((p1 - p)^2 + (p2 - p)^2) / ((p1 * (1 - p1) + p2 * (1 - p2)) / 2))
+geno$fst <- with(geno, ((p1-p)^2 + (p2-p)^2)/(2*p*(1-p)) )
 
 # Replace NaN or infinite values (in case of division by zero)
 geno$fst[is.na(geno$fst) | is.infinite(geno$fst)] <- 0
@@ -58,3 +52,11 @@ write.table(geno, "/work/agro932/bpeng4/PopGen_HW/cache/fst.csv", sep=",", row.n
 fst <- read.csv("/work/agro932/bpeng4/PopGen_HW/cache/fst.csv")
 
 plot(fst$pos, fst$fst, xlab="Physical position", ylab="Fst value", main="")
+
+# Create histogram of Fst values
+ggplot(geno, aes(x = fst)) +
+  geom_histogram(binwidth = 0.05, fill = "blue", color = "black", alpha = 0.7) +
+  labs(title = "Distribution of Fst Values",
+       x = "Fst",
+       y = "Frequency") +
+  theme_minimal()
